50306f5b2a3b751fc97387babbb43cffd8f1bd5c
Kevin Manley <kevin.manley+github@gmail.com>
2014-07-03 13:40:30 -0400

    added more simulation tests

--- simulation/simulation.go
+++ simulation/simulation.go
@@ -1,3 +1,14 @@
+/* other scenarios to test2JobsBasic()
+- abandoned client timeout
+- worker killed
+- job timeout
+- task timeout
+- worker can't handle task and asks for realloc - ensure not assigned to same worker(s) again
+- worker name regex
+- check other Job struct fields for ideas
+
+*/
+
 package main
 
 import (
@@ -10,32 +21,41 @@ import (
 
 var wg sync.WaitGroup
 var quit = make(chan bool)
+var newjob = make(chan grid.JobID)
 
 func simulate(numWorkers int, jobIDs []grid.JobID) {
 	runWorkers(numWorkers)
 	fmt.Println("client:start")
 
-	doneMap := make(map[grid.JobID]bool)
+	jobMap := make(map[grid.JobID]bool)
 	for i := range jobIDs {
-		doneMap[jobIDs[i]] = false
+		jobMap[jobIDs[i]] = false
 	}
 
 	for {
-		if len(doneMap) < 1 {
+		var newJobID grid.JobID
+		select {
+		case newJobID = <-newjob:
+			fmt.Println("client: got new job", newJobID)
+			jobMap[newJobID] = false
+		default:
+		}
+
+		if len(jobMap) < 1 {
 			// no more jobs to check
 			break
 		}
-		for jobID, _ := range doneMap {
+		for jobID, _ := range jobMap {
 			res, err := grid.GetJobResult(jobID)
 			if err == nil {
 				fmt.Println("client: job", jobID, "finished:", res)
-				delete(doneMap, jobID)
+				delete(jobMap, jobID)
 			} else {
 				if e, notFinished := err.(*grid.JobNotFinished); notFinished {
 					fmt.Println("client: job", jobID, e.Error())
 				} else {
 					fmt.Println("client: job", jobID, "failed", err.Error())
-					delete(doneMap, jobID)
+					delete(jobMap, jobID)
 				}
 			}
 		}
@@ -53,14 +73,14 @@ func worker(name string) {
 	defer fmt.Println(name, "end")
 START:
 	for {
+		select {
+		case <-quit:
+			return
+		default:
+		}
 		task := grid.GetTaskForWorker(name)
 		if task == nil {
 			fmt.Println(name, "no tasks available")
-			select {
-			case <-quit:
-				return
-			default:
-			}
 			time.Sleep(3 * time.Second)
 			continue
 		}
@@ -69,10 +89,10 @@ START:
 		// simulate the task taking some time to compute, during which
 		// time we occasionally check status
 		for i := 0; i < 3; i++ {
-			time.Sleep(150 * time.Millisecond)
+			time.Sleep(300 * time.Millisecond)
 			err := grid.CheckJobStatus(name, task.Job, task.Seq)
 			if err != nil {
-				fmt.Println(name, "got check status error", err)
+				fmt.Println(name, "working on", task.Job, task.Seq, "got check status error", err)
 				goto START
 			}
 		}
@@ -140,6 +160,51 @@ func testJobWithGracefulSuspendResume() {
 	simulate(3, []grid.JobID{job1})
 }
 
+func testJobWithGracelessSuspendResume() {
+	job1, _ := grid.CreateJob(&grid.JobDefinition{ID: "job1", Cmd: "", Data: []interface{}{1, 3, 5, 7, 9, 11, 13, 15, 17},
+		Description: "", Ctx: &grid.Context{"foo": "bar"},
+		Ctrl: &grid.JobControl{MaxConcurrency: 0}})
+	time.AfterFunc(300*time.Millisecond, func() { fmt.Println("suspending job", job1); grid.SuspendJob(job1, false) })
+	time.AfterFunc(3*time.Second, func() { fmt.Println("resuming job", job1); grid.RetryJob(job1) })
+	simulate(7, []grid.JobID{job1})
+}
+
+func testPreemptConcurrency() {
+	job1, _ := grid.CreateJob(&grid.JobDefinition{ID: "job1", Cmd: "", Data: []interface{}{1, 3, 5, 7, 9, 11, 13, 15, 17},
+		Description: "", Ctx: &grid.Context{"foo": "bar"},
+		Ctrl: &grid.JobControl{MaxConcurrency: 0}})
+	time.AfterFunc(500*time.Millisecond, func() { fmt.Println("changing concurrency to 2", job1); grid.SetJobMaxConcurrency(job1, 2) })
+	simulate(5, []grid.JobID{job1})
+}
+
+func testPreemptPriority() {
+	job1, _ := grid.CreateJob(&grid.JobDefinition{ID: "job1", Cmd: "", Data: []interface{}{1, 3, 5, 7, 9, 11, 13, 15, 17},
+		Description: "", Ctx: &grid.Context{"foo": "bar"},
+		Ctrl: &grid.JobControl{MaxConcurrency: 0}})
+	time.AfterFunc(1*time.Second, func() {
+		fmt.Println("adding new higher priority job job2")
+		job2, _ := grid.CreateJob(&grid.JobDefinition{ID: "job2", Cmd: "", Data: []interface{}{-1, -2, -3, -4, -5, -6},
+			Description: "", Ctx: &grid.Context{"foo": "bar"},
+			Ctrl: &grid.JobControl{JobPriority: 100}})
+		newjob <- job2
+	})
+	simulate(5, []grid.JobID{job1})
+}
+
+func testJobFutureStartTime() {
+	job1, _ := grid.CreateJob(&grid.JobDefinition{ID: "job1", Cmd: "", Data: []interface{}{1, 3, 5, 7, 9, 11, 13, 15, 17},
+		Description: "", Ctx: &grid.Context{"foo": "bar"},
+		Ctrl: &grid.JobControl{StartTime: time.Now().Add(2 * time.Second), MaxConcurrency: 0}})
+	simulate(5, []grid.JobID{job1})
+}
+
+func testJobWithWorkerNameRegex() {
+	job1, _ := grid.CreateJob(&grid.JobDefinition{ID: "job1", Cmd: "", Data: []interface{}{1, 3, 5, 7, 9, 11, 13, 15, 17},
+		Description: "", Ctx: &grid.Context{"foo": "bar"},
+		Ctrl: &grid.JobControl{WorkerNameRegex: "worker0|worker3", MaxConcurrency: 0}})
+	simulate(5, []grid.JobID{job1})
+}
+
 func main() {
 	numcpu := runtime.NumCPU()
 	fmt.Println("setting GOMAXPROCS to", numcpu)
@@ -149,7 +214,12 @@ func main() {
 	//test2JobsBasic()
 	//test2JobsWithPriority()
 	//test2JobsWithCancel()
-	testJobWithGracefulSuspendResume()
+	//testJobWithGracefulSuspendResume()
+	//testJobWithGracelessSuspendResume()
+	//testChangeConcurrency()
+	//testPreemptPriority()
+	//testJobFutureStartTime()
+	testJobWithWorkerNameRegex()
 
 	wg.Wait()
 }
